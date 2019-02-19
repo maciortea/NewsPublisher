4f5438d1e719f38e2a85e0a9fa415a4a7eb1c748
Marian Ciortea <marian.ciortea@yahoo.com>
2019-02-19 19:54:14 +0200

    Seed database with users.

--- src/Infrastructure/ApplicationDbContext.cs
+++ src/Infrastructure/ApplicationDbContext.cs
@@ -1,9 +1,7 @@
 ﻿using ApplicationCore.Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Infrastructure
 {
@@ -14,6 +12,20 @@ namespace Infrastructure
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Article>(ConfigureArticle);
+        }
+
+        private void ConfigureArticle(EntityTypeBuilder<Article> builder)
+        {
+            builder.Property(a => a.Title).HasMaxLength(100).IsRequired();
+            builder.Property(a => a.Body).IsRequired();
+            builder.Property(a => a.AuthorId).IsRequired();
         }
     }
 }
---
+++ src/Infrastructure/ApplicationDbContextSeed.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public sealed class ApplicationDbContextSeed
+    {
+        public static async Task SeedAsync(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            string publisherUserId = await EnsureUserAsync(userManager, "publisher@test.com", "P@ssword1");
+            string userId = await EnsureUserAsync(userManager, "test@test.com", "P@ssword1");
+            await EnsureRoleAsync(userManager, roleManager, "Publisher", publisherUserId);
+            await EnsureRoleAsync(userManager, roleManager, "User", userId);
+        }
+
+        private static async Task<string> EnsureUserAsync(UserManager<IdentityUser> userManager, string userName, string password)
+        {
+            var user = await userManager.FindByNameAsync(userName);
+            if (user == null)
+            {
+                user = new IdentityUser { UserName = userName, Email = userName };
+                await userManager.CreateAsync(user, password);
+            }
+            return user.Id;
+        }
+
+        private static async Task EnsureRoleAsync(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager, string roleName, string userId)
+        {
+            if (!await roleManager.RoleExistsAsync(roleName))
+            {
+                await roleManager.CreateAsync(new IdentityRole(roleName));
+            }
+
+            var user = await userManager.FindByIdAsync(userId);
+            await userManager.AddToRoleAsync(user, roleName);
+        }
+    }
+}
